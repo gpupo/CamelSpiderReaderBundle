12068db322256f392e43195a671cc152a639d923
Gilmar Pupo <gilmar@gilmar.eti.br>
2011-11-01 15:46:51 -0200

    menu

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -7,9 +7,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('GpupoCamelSpiderReaderBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('GpupoCamelSpiderReaderBundle:Default:index.html.twig');
+    }
+
+    public function folderAction($type, $id)
+    {
+        return $this->render('GpupoCamelSpiderReaderBundle:Default:index.html.twig');
+    }
+
+    public function getMenuAction($type)
+    {
+
+        $collection = $this->doctrineRegistry
+            ->getRepository('GpupoCamelSpiderBundle:Subscription')
+            ->findByActiveSubscriptions();
+
+        return $this->render('GpupoCamelSpiderReaderBundle:Default:menu.html.twig', array('collection'=> $collection));
     }
 }
