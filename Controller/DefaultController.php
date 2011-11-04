820f551442fb98dcc177361ae453deff3ce1062e
Gilmar Pupo <gilmar@gilmar.eti.br>
2011-11-03 22:19:05 -0200

    strip tags for PDF

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,7 +4,8 @@ namespace Gpupo\CamelSpiderReaderBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller,
     Symfony\Component\HttpFoundation\Request,
-    Ps\PdfBundle\Annotation\Pdf;
+    Ps\PdfBundle\Annotation\Pdf,
+    PHPPdf\Parser\Exception\ParseException;
 
 class DefaultController extends Controller {
 
@@ -23,6 +24,8 @@ class DefaultController extends Controller {
     }
 
     /**
+     * Print or PDF
+     *
      * @Pdf()
      */
     public function newsAction($news_id, Request $request)
@@ -30,8 +33,14 @@ class DefaultController extends Controller {
         $news = $this->getNewsRepository()
             ->findOneById($news_id);
         $format = $request->get('_format', 'html');
+        try {
+            $this->get('logger')->info($news->getContentToPdf());
+            return $this->render(sprintf('GpupoCamelSpiderReaderBundle:Default:news.%s.twig', $format), array('news' => $news));
+        } catch (ParseException $e) {
+            echo $e->message();
+            return true;
+        }
 
-        return $this->render(sprintf('GpupoCamelSpiderReaderBundle:Default:news.%s.twig', $format), array('news' => $news));
     }
 
     public function folderAction($type, $id)
